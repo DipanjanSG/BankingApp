8abc8675c2d70ac7e484f41fa0710628822640e5
dipanjan.sen.gupta1993@gmail.com <dipanjanlocal@localpc.com>
2020-06-25 10:51:38 +0530

    changed linux sheel scrpt

--- LinuxBuild.sh
+++ LinuxBuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Where do you want to deploy the .war file? Enter path, else hit ENTER if you want in target directory--->";
 read filePath
+if [ ${#filePath} -eq 0]
+then
+filePath="$(pwd)/target"
+fi
 echo "YOUR .war FILE WILL BE SENT TO $filePath"
-#if [ ${#filePath} -eq 0] && filePath="$(pwd)/target";
 mvn clean install "-DbuildDirectory=$filePath"
